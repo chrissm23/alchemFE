efe85f9e8446b2138079355afc0828c5acf59e35
chrissm23 <ccsm983@gmail.com>
2021-08-16 17:07:21 +0200

    Added probably necessary files

---
+++ alchemFE.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pymbar
+
+import parse_FE_AMBER
+import calculate_TI
+import calculate_MBAR
+
+# Give directory containing subdirectories for all windows
+dir_simulations = '../simulations'
